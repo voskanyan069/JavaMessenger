c4d60bf3fc64b79ffb468a7ee59cbc1b0aeb522c
Andranik Voskanyan <voskanyan069@gmail.com>
2021-01-02 17:11:49 +0400

    delete unnecessary variable, constructor and function from user class

--- src/sample/User.java
+++ src/sample/User.java
@@ -4,12 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User {
-    private final String username;
     private final static List<String> usernames = new ArrayList<>();
-
-    public User(String username) {
-        this.username = username;
-    }
 
     public static void addUsernameToList(String username) {
         usernames.add(username);
@@ -17,10 +12,6 @@ public class User {
 
     public static List<String> getUsernames() {
         return usernames;
-    }
-
-    public String getUsername() {
-        return username;
     }
 
     public static void clearList() {
